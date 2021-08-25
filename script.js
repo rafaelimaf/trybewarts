317db7e672eb9b2f6455e8676200829e09b36524
Rafael de Lima <rafaelima.fernandez@gmail.com>
2021-08-25 16:53:49 -0300

    tentativa requisito 3, adicionar alerta

---
+++ script.js
@@ -0,0 +1,13 @@
+// function alert() {
+//   const button = document.querySelector('#btn-login');
+//   const verifyEmail = 'tryber@teste.com';
+//   const verifyPassword = '123456';
+
+//   let inputEmail = document.querySelector('#input-email');
+//   let inputPassword = document.querySelector('#input-password');
+
+//   button.addEventListener('click', clicou() {
+//   });
+// }
+
+// alert();
