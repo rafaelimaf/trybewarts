1c64ce8674ebbb538149b63737e35e3ae4a74e75
Rafael de Lima <rafaelima.fernandez@gmail.com>
2021-08-25 18:10:58 -0300

    adiciona requisitos 8 e 9

--- script.js
+++ script.js
@@ -3,8 +3,8 @@
 //   const verifyEmail = 'tryber@teste.com';
 //   const verifyPassword = '123456';
 
-//   let inputEmail = document.querySelector('#input-email');
-//   let inputPassword = document.querySelector('#input-password');
+//   let inputEmail = document.querySelector('#login-email');
+//   let inputPassword = document.querySelector('#login-password');
 
 //   button.addEventListener('click', clicou() {
 //   });
