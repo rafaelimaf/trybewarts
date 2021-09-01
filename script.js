55756f48d0f7251cdc97e935de3006362b9ff06a
Valdeci <moura97.valdeci@gmail.com>
2021-09-01 15:54:16 -0300

    correção lint do requisito 3

--- script.js
+++ script.js
@@ -1,10 +1,7 @@
 window.onload = () => {
   const button = document.querySelector('#btn-login');
-  //Seleciona o botão criado no html
   const loginInfo = ['tryber@teste.com', '123456'];
-  //Lista com informações de login
-  let inputLogin = document.querySelector('#infoLogin');
-  //Seleciona formulário com elementos de login. (email, etc)
+  const inputLogin = document.querySelector('#infoLogin');
   
   const loginConected = () => {
     //Percorre o campo com elementos de login e os compara com a nova variável infoInput.
@@ -19,8 +16,8 @@ window.onload = () => {
   }
 
   button.addEventListener('click', loginConected);
-}
+};
 
-//Usei como referência esse site: "https://pt.stackoverflow.com/questions/207822/cannot-read-property-addeventlistener-of-null". 
-//Usado para aplicar o window.onload já o script foi colocado no início do html e sempre retornava o erro Cannot read property 'addEventListener' of null ao usar no button.
-//Usei também esse site: "https://developer.mozilla.org/pt-BR/docs/Learn/JavaScript/Building_blocks/Build_your_own_function"; para me auxiliar na lógica de como montar uma função pegando elementos a partir do DOM de modo geral.
+// Usei como referência esse site: "https://pt.stackoverflow.com/questions/207822/cannot-read-property-addeventlistener-of-null". 
+// Usado para aplicar o window.onload já o script foi colocado no início do html e sempre retornava o erro Cannot read property 'addEventListener' of null ao usar no button.
+// Usei também esse site: "https://developer.mozilla.org/pt-BR/docs/Learn/JavaScript/Building_blocks/Build_your_own_function"; para me auxiliar na lógica de como montar uma função pegando elementos a partir do DOM de modo geral.
