b4ec79b06b63f5a23138a5cc07bdc7a228f9a225
Valdeci <moura97.valdeci@gmail.com>
2021-09-01 15:58:57 -0300

    correção erros de lint requisito 3

--- script.js
+++ script.js
@@ -2,9 +2,9 @@ window.onload = () => {
   const button = document.querySelector('#btn-login');
   const loginInfo = ['tryber@teste.com', '123456'];
   const inputLogin = document.querySelector('#infoLogin');
-  
+
   const loginConected = () => {
-    //Percorre o campo com elementos de login e os compara com a nova variável infoInput.
+    // Percorre o campo com elementos de login e os compara com a nova variável infoInput.
     for (let i = 0; i < inputLogin.length - 1; i += 1) {
       const infoInput = inputLogin[i];
       if (infoInput.value !== loginInfo[i]) {
@@ -13,11 +13,11 @@ window.onload = () => {
       }
     }
     alert('Olá, Tryber!');
-  }
+  };
 
   button.addEventListener('click', loginConected);
 };
 
-// Usei como referência esse site: "https://pt.stackoverflow.com/questions/207822/cannot-read-property-addeventlistener-of-null". 
+// Usei como referência esse site: "https://pt.stackoverflow.com/questions/207822 cannot-read-property-addeventlistener-of-null".
 // Usado para aplicar o window.onload já o script foi colocado no início do html e sempre retornava o erro Cannot read property 'addEventListener' of null ao usar no button.
 // Usei também esse site: "https://developer.mozilla.org/pt-BR/docs/Learn/JavaScript/Building_blocks/Build_your_own_function"; para me auxiliar na lógica de como montar uma função pegando elementos a partir do DOM de modo geral.
